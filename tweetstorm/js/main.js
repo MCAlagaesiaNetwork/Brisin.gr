90b4c6600a352b8e9406fcf9f33f2be4c7a73242
Jack Williams <williams-jack@outlook.com>
2021-05-17 03:02:03 +1000

    Test

--- tweetstorm/js/main.js
+++ tweetstorm/js/main.js
@@ -1,33 +1,3 @@
-(function () {
-  const second = 1000,
-        minute = second * 60,
-        hour = minute * 60,
-        day = hour * 24;
-
-  let tweetstorm = "Saturday, 19 June 2021 15:00:00 UTC",
-      countDown = new Date(tweetstorm).getTime(),
-      x = setInterval(function() {    
-
-        let now = new Date().getTime(),
-            distance = countDown - now;
-
-        document.getElementById("days").innerText = Math.floor(distance / (day)),
-        document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-        document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-        document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-
-        //do something later when date is reached
-        if (distance < 0) {
-          let countdown = document.getElementById("countdown"),
-              content = document.getElementById("message");
-          countdown.style.display = "none";
-          content.style.display = "block";
-
-          clearInterval(x);
-        }
-        //seconds
-      }, 0)
-  }());
 (function($) {
   
   "use strict";
